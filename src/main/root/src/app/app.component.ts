4edac9217b49fa89b639637652a3e1fa86be3046
Upendra Vemulapalli <uvemulapalli@gmail.com>
2023-03-24 03:42:30 +0530

    With Mat-Sort.

--- src/main/root/src/app/app.component.ts
+++ src/main/root/src/app/app.component.ts
@@ -64,7 +64,7 @@ export class AppComponent {
 		this.dataSource.filter = filterValue.trim().toLowerCase();
 	}
 
-	loadInstruments(): void {
+	private loadInstruments(): void {
     this.uploadService.loadAllActiveInstruments().subscribe({
       next: (event: any) => {
         if (event instanceof HttpResponse) {
