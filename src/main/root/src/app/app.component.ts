a3ef0f52b5952d39493485990b10f101b54583ea
Sarath Nandigam <sarath.rsrl@gmail.com>
2023-03-24 14:41:38 +0530

    Updates

--- src/main/root/src/app/app.component.ts
+++ src/main/root/src/app/app.component.ts
@@ -2,12 +2,29 @@ import { Component, OnInit, AfterViewInit, Injectable, ViewChild, ViewEncapsulat
 import { MatTable, MatTableDataSource } from '@angular/material/table';
 import { MatFormField, MatFormFieldControl } from "@angular/material/form-field";
 import { HttpEventType, HttpResponse } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, of, min, max } from 'rxjs';
+import { filter, map } from 'rxjs/operators';
 import { FileUploadService } from 'src/app/services/file-upload.service';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTabChangeEvent } from '@angular/material/tabs';
 import { MatButtonModule } from "@angular/material/button";
+
+export class DataDisplayResponseType {
+  errorMessage: string = '';
+  dataDisplayResponse: any = [];
+}
+
+export class DataDisplayResponse {
+  ticker: string = '';
+  spotPrice: number = 0;
+  strikePrice: number = 0;
+  expiry: number = 0;
+  volatility: number = 0;
+  optionPrice: number = 0;
+  predictedPrice: number = 0;
+  timeTaken: number = 0;
+}
 
 @Component({
   selector: 'app-root',
@@ -18,46 +35,49 @@ import { MatButtonModule } from "@angular/material/button";
 
 export class AppComponent implements OnInit, AfterViewInit {
 
-	title = 'Derivative Prices';
+  title = 'Derivative Prices';
 
-	tab1Title = 'Simulation';
+  tab1Title = 'Simulation';
 
-	tab2Title = 'Graph';
+  tab2Title = 'Graph';
 
-	displayTab1 = true;
+  displayTab1 = true;
 
-	displayTab2 = false;
+  displayTab2 = false;
 
-	tab: number = 2;
+  tab: number = 2;
 
-	dataDisplayResponseType: DataDisplayResponseType = new DataDisplayResponseType();
+  dataDisplayResponseType: DataDisplayResponseType = new DataDisplayResponseType();
 
-	displayedColumns: string[] = [];
+  displayedColumns: string[] = [];
 
-	dataSource: any;
+  dataSource: any;
 
-	private baseUrl: string = 'http://localhost:5080';
+  private baseUrl: string = 'http://localhost:5080';
 
-	message: string = '';
+  message: string = '';
 
-	errorMessage: string = '';
+  errorMessage: string = '';
 
-	@ViewChild(MatSort, {static: false}) set matSort(sort: MatSort) {
-      if (this.dataSource && !this.dataSource.sort) {
-          this.dataSource.sort = sort;
-      }
+  minStrikePrice = 0.0;
+  maxStrikePrice = 0.0;
+
+  @ViewChild(MatSort, { static: false }) set matSort(sort: MatSort) {
+    if (this.dataSource && !this.dataSource.sort) {
+      this.dataSource.sort = sort;
+    }
   }
 
-	@ViewChild(MatPaginator, {static: false}) set paginator(paginator: MatPaginator) {
-	  if (this.dataSource && !this.dataSource.paginator) {
+  @ViewChild(MatPaginator, { static: false }) set paginator(paginator: MatPaginator) {
+    if (this.dataSource && !this.dataSource.paginator) {
       this.dataSource.paginator = paginator;
     }
-	}
+  }
 
-	constructor(private uploadService: FileUploadService) {
+  constructor(private uploadService: FileUploadService) {
     this.displayTab1 = true;
     this.displayTab2 = false;
-	}
+  }
 
   ngOnInit(): void {
     this.loadInstruments();
@@ -65,22 +85,26 @@ export class AppComponent implements OnInit, AfterViewInit {
 
   ngAfterViewInit(): void { }
 
-	applyFilter(event: Event) {
-		const filterValue = (event.target as HTMLInputElement).value;
-		this.dataSource.filter = filterValue.trim().toLowerCase();
-	}
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
 
-	private loadInstruments(): void {
+  private loadInstruments(): void {
     this.uploadService.loadAllActiveInstruments().subscribe({
       next: (event: any) => {
         if (event instanceof HttpResponse) {
           this.dataDisplayResponseType = event.body;
-          if(this.dataDisplayResponseType.errorMessage){
+          if (this.dataDisplayResponseType.errorMessage) {
             this.errorMessage = this.dataDisplayResponseType.errorMessage;
           } else {
             var response = this.dataDisplayResponseType.dataDisplayResponse;
             this.dataSource = new MatTableDataSource(response);
+            this.dataSource.data.forEach((element: any) => {
+              element['cellHighlightColor'] = 'default-cell';
+            });
             this.displayedColumns = Object.keys(response[0]);
+            this.findMinMaxStrikePrice();
           }
         }
       },
@@ -93,36 +117,57 @@ export class AppComponent implements OnInit, AfterViewInit {
         }
       }
     });
-	}
+  }
 
   onChange(event: MatTabChangeEvent) {
     const tab = event.tab.textLabel;
     console.log(tab);
-    if(tab === this.tab1Title) {
+    if (tab === this.tab1Title) {
       this.displayTab1 = true;
       this.displayTab2 = false;
       this.loadInstruments();
     }
 
-    if(tab === this.tab2Title) {
+    if (tab === this.tab2Title) {
       this.displayTab1 = false;
       this.displayTab2 = true;
     }
   }
+
+  generateRandom(objectToModify: any) {
+    let randomGen = Math.random() * ((9) - (1) + 1);
+    let incDecValue = Math.floor(randomGen);
+    objectToModify.strikePrice = parseFloat((objectToModify.strikePrice + (incDecValue % 2 === 0 ? -randomGen : randomGen)).toFixed(5))
+    return objectToModify;
+  }
+
+  findMinMaxStrikePrice() {
+    if (this.dataSource.data.length) {
+      this.minStrikePrice = Math.min(...this.dataSource.data.map((item: any) => parseFloat(item.strikePrice)));
+      this.maxStrikePrice = Math.max(...this.dataSource.data.map((item: any) => parseFloat(item.strikePrice)));
+      console.log(" Min: " + this.minStrikePrice);
+      console.log(" Max: " + this.maxStrikePrice);
+      this.dataSource.data.forEach((record: any) => {
+        let oldValue = record.strikePrice;
+        setInterval(() => {
+          record = this.generateRandom(record);
+          this.handleHighlight(record, oldValue);
+        }, 3000);
+      });
+    }
+  }
+
+  handleHighlight(row: any, thresholdValue: any) {
+    let redColor = 'red-cell';
+    let greenColor = 'green-cell';
+    if (row.strikePrice < thresholdValue) {
+      row['cellHighlightColor'] = redColor;
+    } else {
+      row['cellHighlightColor'] = greenColor;
+    }
+    row.predictedPrice = row.strikePrice;
+  }
+
 }
 
-export class DataDisplayResponseType {
-	errorMessage: string = '';
-	dataDisplayResponse: any = [];
-}
 
-export class DataDisplayResponse {
-	ticker: string = '';
-	spotPrice: number = 0;
-	strikePrice: number = 0;
-	expiry: number = 0;
-	volatility: number = 0;
-	optionPrice: number = 0;
-	predictedPrice: number = 0;
-	timeTaken: number = 0;
-}
