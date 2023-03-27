7c8fea237eb00fcf4a0084886a5e432614e2e66e
Upendra Vemulapalli <uvemulapalli@gmail.com>
2023-03-27 11:45:04 +0530

    Graphical representation of report.

--- src/main/root/src/app/app.component.ts
+++ src/main/root/src/app/app.component.ts
@@ -125,12 +125,14 @@ export class AppComponent implements OnInit, AfterViewInit {
       this.displayTab2 = false;
       this.loadInstruments();
       this.instrumentProgress = '';
+      this.displayTab3 = false;
     }
     if (tab === this.tab2Title) {
       this.dataSource = undefined;
       this.displayTab1 = false;
       this.displayTab2 = true;
       this.isSimulationEnabled = false;
+      this.displayTab3 = false;
     }
     if (tab === this.tab3Title) {
       this.dataSource = undefined;
@@ -138,6 +140,7 @@ export class AppComponent implements OnInit, AfterViewInit {
       this.displayTab2 = false;
       this.isSimulationEnabled = false;
       this.instrumentProgress = '';
+      this.displayTab3 = true;
     }
   }
 
@@ -364,4 +367,78 @@ export class AppComponent implements OnInit, AfterViewInit {
   }
 
   instrumentProgress: string = '';
+
+  chart: any;
+
+  chartOptions = {
+    animationEnabled: true,
+    theme: "light2",
+    title:{
+    text: "Actual vs Blocksholes"
+    },
+    axisX:{
+    valueFormatString: "D MMM"
+    },
+    axisY: {
+    title: "Spot Prices"
+    },
+    toolTip: {
+    shared: true
+    },
+    legend: {
+    cursor: "pointer",
+    itemclick: function (e: any) {
+      if (typeof (e.dataSeries.visible) === "undefined" || e.dataSeries.visible) {
+        e.dataSeries.visible = false;
+      } else {
+        e.dataSeries.visible = true;
+      }
+      e.chart.render();
+    }
+    },
+    data: [{
+    type: "line",
+    showInLegend: true,
+    name: "Blocksholes Price",
+    xValueFormatString: "MMM DD, YYYY",
+    dataPoints: [
+      { x: new Date(2021, 8, 1), y: 63 },
+      { x: new Date(2021, 8, 2), y: 69 },
+      { x: new Date(2021, 8, 3), y: 65 },
+      { x: new Date(2021, 8, 4), y: 70 },
+      { x: new Date(2021, 8, 5), y: 71 },
+      { x: new Date(2021, 8, 6), y: 65 },
+      { x: new Date(2021, 8, 7), y: 73 },
+      { x: new Date(2021, 8, 8), y: 86 },
+      { x: new Date(2021, 8, 9), y: 74 },
+      { x: new Date(2021, 8, 10), y: 75 },
+      { x: new Date(2021, 8, 11), y: 76 },
+      { x: new Date(2021, 8, 12), y: 84 },
+      { x: new Date(2021, 8, 13), y: 87 },
+      { x: new Date(2021, 8, 14), y: 76 },
+      { x: new Date(2021, 8, 15), y: 79 }
+    ]
+    }, {
+    type: "line",
+    showInLegend: true,
+    name: "Predicted Price",
+    dataPoints: [
+      { x: new Date(2021, 8, 1), y: 60 },
+      { x: new Date(2021, 8, 2), y: 57 },
+      { x: new Date(2021, 8, 3), y: 51 },
+      { x: new Date(2021, 8, 4), y: 56 },
+      { x: new Date(2021, 8, 5), y: 54 },
+      { x: new Date(2021, 8, 6), y: 55 },
+      { x: new Date(2021, 8, 7), y: 54 },
+      { x: new Date(2021, 8, 8), y: 69 },
+      { x: new Date(2021, 8, 9), y: 65 },
+      { x: new Date(2021, 8, 10), y: 66 },
+      { x: new Date(2021, 8, 11), y: 63 },
+      { x: new Date(2021, 8, 12), y: 67 },
+      { x: new Date(2021, 8, 13), y: 66 },
+      { x: new Date(2021, 8, 14), y: 56 },
+      { x: new Date(2021, 8, 15), y: 64 }
+    ]
+    }]
+  }
 }
